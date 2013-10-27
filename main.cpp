d5c3ab6df08a9b84a74de608ecffa4c958f41f75
Oliver Lau <oliver@von-und-fuer-lau.de>
2013-10-27 13:37:30 +0100

    WIN32-Version mit MMIO statt Lesen und Schreiben eines Hauptspeicherpuffers.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <Windows.h>
 #endif
 
+#include <cmath>
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
@@ -48,29 +49,127 @@ static char *infile = NULL;
 static char *outfile = NULL;
 static double percent = 10;
 static ALGORITHMS algorithm = ALGORITHM_XOR;
+static unsigned int BUFFSIZE = 1024;
+
+template <typename T> inline T MIN(T x, T y) { return (x > y) ? y : x; }
+template <typename T> inline T MAX(T x, T y) { return (x < y) ? y : x; }
+
+static inline bool fuzzyEqual(double x, double y) {
+  return fabs(x - y) <= (0.000000000001 * MIN(fabs(x), fabs(y)));
+}
+
+
+double random(double x, double y) {
+  if (fuzzyEqual(x, y))
+    return x;
+  double hi = MAX(x, y), lo = MIN(x, y);
+  return lo + ((hi - lo) * rand() / (RAND_MAX + 1.0));
+}
 
 
 void usage() {
   printf("Usage: glitch --in original.jpg --out glitched.jpg [--iterations|-i %d] [--percent|-p %lf] [--algorithm|-a XOR|OR]\n\r", iterations, percent);
 }
 
-#define MIN(x, y) (x)>(y)? (y):(x)
-#define MAX(x, y) (x)<(y)? (y):(x)
 
-double random(double x, double y) {
-  double high = 0;
-  double low = 0;
-  double randNum = 0;
-  // if there is no range, return the value
-  if (x == y) return x; 			// float == ?, wise? epsilon?
-  high = MAX(x, y);
-  low = MIN(x, y);
-  randNum = low + ((high-low) * rand()/(RAND_MAX + 1.0));
-  return randNum;
+#ifdef WIN32
+void glitch(void) {
+  srand(GetTickCount());
+  BOOL rc = CopyFile(infile, outfile, FALSE);
+  if (rc == 0) {
+    printf(TEXT("Copying failed, last error: %d\n"), GetLastError());
+    exit(1);
+  }
+  HANDLE hFile = CreateFile(outfile, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (hFile == INVALID_HANDLE_VALUE) {
+    printf(TEXT("hFile is NULL\n"));
+    printf(TEXT("Target file is %s\n"),
+             outfile);
+    exit(4);
+  }
+  DWORD dwFileSize = GetFileSize(hFile,  NULL);
+  SYSTEM_INFO SysInfo;  // system information; used to get granularity
+  GetSystemInfo(&SysInfo);
+  DWORD dwSysGran = SysInfo.dwAllocationGranularity;
+  DWORD dwFirstPos = (DWORD)(1e-2 * dwFileSize * percent);
+  DWORD dwFileMapStart = (dwFirstPos / dwSysGran) * dwSysGran;
+  DWORD dwMapViewSize = dwSysGran;
+  DWORD wFileMapSize = dwFileSize;
+  HANDLE hMapFile = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, 0, NULL);
+  if (hMapFile == NULL) {
+    printf(TEXT("hMapFile is NULL, last error: %d\n"), GetLastError() );
+    exit(2);
+  }
+  printf("in between %lu and %lu\n", dwFirstPos, dwFileSize); 
+  for (int i = 0; i < iterations; ++i) {
+    DWORD dwPos = (DWORD) random(dwFirstPos, dwFileSize);
+    LPVOID lpMapAddress = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, dwFileMapStart, 0);
+    if (lpMapAddress == NULL) {
+      printf(TEXT("lpMapAddress is NULL, last error: %d\n"), GetLastError());
+      exit(3);
+    }
+    BYTE *b = (BYTE*)lpMapAddress + dwPos;
+    INT bit = rand() % 8;
+    BYTE oldByte = *b;
+    BYTE newByte = oldByte;
+    switch (algorithm) {
+    default:
+      // fall-through
+    case ALGORITHM_XOR:
+      newByte ^= (1 << bit);
+      break;
+    case ALGORITHM_OR:
+      newByte |= (1 << bit);
+      break;
+    }
+    *b = newByte;
+    printf("glitching at position %lu:%d (%02xh->%02xh)\n", dwPos, bit, (UINT)oldByte, (UINT)newByte);
+    UnmapViewOfFile(lpMapAddress);
+  }
+  CloseHandle(hMapFile);
+  CloseHandle(hFile);
 }
+#else
+void glitch(void) {
+  FILE *in = fopen(infile, "rb");
+  fseek(in, 0L, SEEK_END);
+  long sz = ftell(in);
+  fseek(in, 0L, SEEK_SET);
+  unsigned char *buf = (unsigned char*) malloc(sz);
+  fread(buf, 1, sz, in);
+  fclose(in);
+  FILE *out = fopen(outfile, "wb+");
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  long int n = (tv.tv_sec ^ tv.tv_usec) ^ getpid();
+  srand(n);
+  long firstPos = (long)(1e-2 * sz * percent);
+  printf("in between %lu and %lu\n\r", firstPos, sz); 
+  for (int i = 0; i < iterations; ++i) {
+    const long pos = (long) random(firstPos, sz);
+    const int bit = rand() % 8;
+    const unsigned char oldByte = buf[pos];
+    unsigned char newByte = oldByte;
+    switch (algorithm) {
+    default:
+      // fall-through
+    case ALGORITHM_XOR:
+      newByte ^= (1 << bit);
+      break;
+    case ALGORITHM_OR:
+      newByte |= (1 << bit);
+      break;
+    }
+    buf[pos] = newByte;
+    printf("glitching at position %ld:%d (%02xh->%02xh)\n", pos, bit, (unsigned int) oldByte, (unsigned int) newByte);
+  }
+  fwrite(buf, 1, sz, out);
+  fclose(out);
+  free(buf);
+}
+#endif
 
 int main(int argc, char* argv[]) {
-  printf("BLAH\n\r");
   for (;;) {
     int option_index = 0;
     int c = getopt_long(argc, argv, "i:p:h?vqa:", long_options, &option_index);
@@ -133,45 +232,7 @@ int main(int argc, char* argv[]) {
     usage();
     return EXIT_FAILURE;
   }
-  printf("%s -> %s\n\r", infile, outfile); 
-  FILE *in = fopen(infile, "rb");
-  fseek(in, 0L, SEEK_END);
-  long sz = ftell(in);
-  fseek(in, 0L, SEEK_SET);
-  unsigned char *buf = (unsigned char*) malloc(sz);
-  fread(buf, 1, sz, in);
-  fclose(in);
-  FILE *out = fopen(outfile, "wb+");
-#ifdef WIN32
-  srand(GetTickCount());
-#else
-  struct timeval tv;
-  gettimeofday(&tv, 0);
-  long int n = (tv.tv_sec ^ tv.tv_usec) ^ getpid();
-  srand(n);
-#endif
-  long firstPos = (long)(1e-2 * sz * percent);
-  printf("in between %lu and %lu\n\r", firstPos, sz); 
-  for (int i = 0; i < iterations; ++i) {
-    const long pos = (long) random(firstPos, sz);
-    const int bit = rand() % 8;
-    const unsigned char oldByte = buf[pos];
-    unsigned char newByte = oldByte;
-    switch (algorithm) {
-    default:
-      // fall-through
-    case ALGORITHM_XOR:
-      newByte ^= (1 << bit);
-      break;
-    case ALGORITHM_OR:
-      newByte |= (1 << bit);
-      break;
-    }
-    buf[pos] = newByte;
-    printf("glitching at position %ld:%d (%02xh->%02xh)\n", pos, bit, (unsigned int) oldByte, (unsigned int) newByte);
-  }
-  fwrite(buf, 1, sz, out);
-  fclose(out);
-  free(buf);
+  printf("%s -> %s\n", infile, outfile); 
+  glitch();
   return EXIT_SUCCESS;
 }
